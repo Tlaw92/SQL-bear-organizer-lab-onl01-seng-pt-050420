5cc53d046958225d43ac99e4822ac7dbed10fb8b
travis Lawson <travislawclimb@gmail.com>
2020-07-03 22:41:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,9 @@
-sqlite> INSERT INTO bears (name, age, gender, color, temperament, alive, owner_id) VALUES ("Mr, Chocolate", 1, "Male", "Brown","Friendly", 1, 1);
+sqlite> INSERT INTO bears (name, age, gender, color, temperament, alive, owner_id) VALUES ("Mr, Chocolate", 1, "Male", "Brown","Friendly", 1,1);
+
 sqlite> INSERT INTO cats (name, age, breed, owner_id) VALUES ("Hana", 1 , "Tabby", 1);
+
 sqlite> INSERT INTO cats (name, age, breed) VALUES ("Lil\' Bub", 5, "American Shorthair");
+
 sqlite> INSERT INTO cats (name, age, breed) VALUES ("Moe", 10, "Tabby");
+
 sqlite> INSERT INTO cats (name, age, breed) VALUES ("Patches", 2, "Calico");
